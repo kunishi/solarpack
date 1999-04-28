3b6e4b41c2fc1805a87e1b654eb01bae860c8cd2
kunishi <devnull@localhost>
1999-04-28 02:47:08 +0000

    added uniq command. replaced the owner kunishi to root, the group dblab to other, to fit the standard root environment on Solaris.

--- src/tools/postproto.sh
+++ src/tools/postproto.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# $Id: postproto.sh,v 1.1 1999/04/27 11:43:19 kunishi Exp $
+# $Id: postproto.sh,v 1.2 1999/04/28 02:47:08 kunishi Exp $
 #
-/usr/bin/sort +2 | \
+/usr/bin/sort +2 | /usr/bin/uniq | \
 /usr/bin/sed \
-	-e 's?^\(f .*\) kunishi dblab?\1 bin bin?' \
-	-e 's?^\(d .*\) kunishi dblab?\1 root bin?' \
+	-e 's?^\(f .*\) \(0[0-9]*\) root other?\1 \2 bin bin?' \
+	-e 's?^\(d .*\) root other?\1 root bin?' \
 	-e 's?^\(. none\) \(.*\) \([0-9]* .*\)?\1 \2=%%WRK_BASEDIR%%/\2 \3?'
