87704fff86fa333dc093edc5b8161a89e6775dd6
kunishi <devnull@localhost>
1999-05-13 12:16:29 +0000

    Modified Files: 	postproto.sh

--- src/tools/postproto.sh
+++ src/tools/postproto.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# $Id: postproto.sh,v 1.2 1999/04/28 02:47:08 kunishi Exp $
+# $Id: postproto.sh,v 1.3 1999/05/13 12:16:29 kunishi Exp $
 #
 /usr/bin/sort +2 | /usr/bin/uniq | \
 /usr/bin/sed \
-	-e 's?^\(f .*\) \(0[0-9]*\) root other?\1 \2 bin bin?' \
+	-e 's?^\(f .*\) \(0[0-9]*\) root other?\1 \2 root bin?' \
 	-e 's?^\(d .*\) root other?\1 root bin?' \
 	-e 's?^\(. none\) \(.*\) \([0-9]* .*\)?\1 \2=%%WRK_BASEDIR%%/\2 \3?'
