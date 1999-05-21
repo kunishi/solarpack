d32e7a2da633ada3097f4e0982c6964dc539575f
kunishi <devnull@localhost>
1999-05-21 02:13:00 +0000

    Modified Files: 	postproto.sh : added the line of 'pkginfo' automatically.

--- src/tools/postproto.sh
+++ src/tools/postproto.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-# $Id: postproto.sh,v 1.3 1999/05/13 12:16:29 kunishi Exp $
+# $Id: postproto.sh,v 1.4 1999/05/21 02:13:00 kunishi Exp $
 #
 /usr/bin/sort +2 | /usr/bin/uniq | \
 /usr/bin/sed \
+	-e '1 i\
+i pkginfo=%%PKGDIR%%/pkginfo' \
 	-e 's?^\(f .*\) \(0[0-9]*\) root other?\1 \2 root bin?' \
 	-e 's?^\(d .*\) root other?\1 root bin?' \
 	-e 's?^\(. none\) \(.*\) \([0-9]* .*\)?\1 \2=%%WRK_BASEDIR%%/\2 \3?'
