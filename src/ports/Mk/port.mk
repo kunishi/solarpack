c6885788ad531af4474a09b426a9531c9c2ddcb9
kunishi <devnull@localhost>
1999-06-03 11:40:46 +0000

    implemented MD5 checking phase.

--- src/ports/Mk/port.mk
+++ src/ports/Mk/port.mk
@@ -1,43 +1,8 @@
 #
-# $Id: port.mk,v 1.16 1999/06/02 11:15:56 kunishi Exp $
+# $Id: port.mk,v 1.17 1999/06/03 11:39:48 kunishi Exp $
 #
 
 .include "/opt/local/pkgbuild/conf/pkgbuild.conf"
-
-CCSMAKE=	/usr/ccs/bin/make
-CHOWN=		/usr/bin/chown
-CP=		/usr/bin/cp
-ECHO=		/usr/bin/echo
-ENV=		/usr/bin/env
-GMAKE=		/usr/local/bin/gmake
-GTAR=		/usr/local/bin/gtar
-GZCAT=		/usr/local/bin/gzip -cd
-GZIP=		/usr/local/bin/gzip
-INSTALL=	/usr/ucb/install
-LN=		/usr/bin/ln
-MV=		/usr/bin/mv
-MKDIR=		/usr/bin/mkdir -p
-PATCH=		/usr/local/bin/patch
-PKGADD=		/usr/sbin/pkgadd
-PKGMK=		/usr/bin/pkgmk -o
-PKGPROTO=	/usr/bin/pkgproto
-PKGTRANS=	/usr/bin/pkgtrans -o
-PWD=		/usr/bin/pwd
-RM=		/usr/bin/rm
-SED=		/usr/bin/sed
-SH=		/bin/sh
-TOUCH=		/usr/bin/touch
-UNAME=		/usr/bin/uname
-WGET=		/usr/local/bin/wget
-
-ECHO_MSG=	${ECHO}
-POSTPROTO=	${PKGBUILDDIR}/tools/postproto.sh
-
-INSTALL_PROGRAM= ${INSTALL} -c -s -m 755 -o root -g bin
-INSTALL_DATA=	${INSTALL} -c -m 644 -o root -g bin
-INSTALL_MAN=	${INSTALL} -c -m 644 -o root -g bin
-INSTALL_SCRIPT=	${INSTALL} -c -m 755 -o root -g bin
-INSTALL_TARGET?=	install
 
 .if !defined(ARCH)
 ARCH!=		/usr/bin/mach
@@ -55,51 +20,39 @@ GNU_HOSTTYPE?=	${ARCH}-sun-solaris${OSREL_SOLARIS}
 GNU_HOSTTYPE?=	${ARCH}--solaris${OSREL_SOLARIS}
 .endif
 
-.if defined(GNU_CONFIGURE)
-HAS_CONFIGURE=		yes
-CONFIGURE_ARGS+=	--prefix=${PREFIX}
-MAKE+ARGS+=		prefix=${PREFIX}
-MAKE_INSTALL_ARGS+=	prefix=${WRK_BASEDIR}
-.endif
+MASTERDIR?=	${.CURDIR}
 
-.if defined(HAS_CONFIGURE)
-CONFIGURE?=	${WRKSRC}/configure
-.endif
+LOCALBASE?=	/usr/local
+X11BASE?=	/usr/openwin
 
-.if defined(USE_GMAKE)
-MAKE_ENV+=	MAKE=${GMAKE}
-.endif
+PKGBUILDDIR?=	/opt/pkgbuild
+DISTDIR?=	${PKGBUILDDIR}/distfiles
+EXTRACT_SUFX?=	.tar.gz
+RELEASE_PKG_DIR?=	${PKGBUILDDIR}/packages
+
+PATCHDIR?=	${MASTERDIR}/patches
+TOOLSDIR?= 	${PKGBUILDDIR}/tools
+FILESDIR?=	${MASTERDIR}/files
+SCRIPTDIR?=	${MASTERDIR}/scripts
+PKGDIR?=	${MASTERDIR}/pkg
 
 .if defined(USE_IMAKE)
 USE_X_PREFIX=	yes
-CONFIGURE?=	${X11BASE}/bin/xmkmf
-CONFIGURE_ARGS+=	-a
-MAKE_ARGS+=	DESTDIR=${PREFIX}
-MAKE_INSTALL_ARGS+=	DESTDIR=${WRKDIR} PREFIX=${PREFIX} \
-		LOCALBASE=${LOCALBASE} X11BASE=${X11BASE}
-.if !defined(NO_INSTALL_MAN)
-INSTALL_TARGET+=	install.man
 .endif
-.endif
-
 .if defined(USE_X_PREFIX)
-PREFIX=		${X11BASE}
+PREFIX?=	${X11BASE}
+.else
+PREFIX?=	${LOCALBASE}
 .endif
 
-LOCALBASE=	/usr/local
-X11BASE=	/usr/openwin
-PREFIX?=	${LOCALBASE}
-
-DISTDIR=	${PKGBUILDDIR}/distfiles
-TOOLSDIR= 	${PKGBUILDDIR}/tools
-FILESDIR=	${.CURDIR}/files
-PATCHDIR=	${.CURDIR}/patches
-SCRIPTDIR=	${.CURDIR}/scripts
-WRKDIR=		${.CURDIR}/work
+WRKDIR?=	${MASTERDIR}/work
 WRKSRC?=	${WRKDIR}/${DISTNAME}
 WRK_BASEDIR?=	${WRKDIR}${PREFIX}
 SPOOLDIR?=	${WRKDIR}/spool
-PKGDIR=		${.CURDIR}/pkg
+
+PROTOTYPE_SUB+=	PKGDIR=${PKGDIR} \
+		WRK_BASEDIR=${WRK_BASEDIR} \
+		GNU_HOSTTYPE=${GNU_HOSTTYPE}
 
 EXTRACT_COOKIE=		${WRKDIR}/.extract_done
 PATCH_COOKIE=		${WRKDIR}/.patch_done
@@ -109,6 +62,104 @@ INSTALL_COOKIE=		${WRKDIR}/.install_done
 PACKAGE_COOKIE=		${WRKDIR}/.package_done
 INSTPKG_COOKIE=		${WRKDIR}/.instpkg_done
 RELEASE_COOKIE=		${WRKDIR}/.release_done
+
+CC?=		gcc
+GMAKE?=		gmake
+XMKMF?=		${X11BASE}/bin/xmkmf -a
+
+CONFIGURE_ENV+=	CC=${CC} \
+		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib
+
+MD5?=		${LOCALBASE}/bin/md5
+MD5_FILE=	${FILESDIR}/md5
+
+MAKEFILE?=	Makefile
+MAKE_FLAGS?=	-f ${MAKEFILE}
+MAKE_ENV+=	PREFIX=${PREFIX} \
+		LOCALBASE=${LOCALBASE} \
+		X11BASE=${X11BASE} \
+		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib \
+		CC=${CC}
+
+MAKE_INSTALL_ENV+=	PREFIX=${WRK_BASEDIR} \
+		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib \
+		CC=${CC}
+MAKE_INSTALL_ARGS+=	INSTALL=${INSTALL}
+MAKE_INSTALL_EXEC_DIR?=	${WRKSRC}
+
+TOUCH?=		/usr/bin/touch
+TOUCH_FLAGS?=	
+
+PATCH?=		${LOCALBASE}/bin/patch
+PATCH_STRIP?=	-p0
+PATCH_DIST_STRIP?=	-p0
+PATCH_ARGS?=	-d ${WRKSRC} --backup --forward --quiet -E ${PATCH_STRIP}
+PATCH_DIST_APPLY_DIR?=	${WRKSRC}
+PATCH_DIST_ARGS?=	-d ${PATCH_DIST_APPLY_DIR} --backup --forward --quiet -E ${PATCH_DIST_STRIP}
+
+TAR?=		gtar
+
+EXTRACT_CMD?=	${GZIP_CMD}
+
+INSTALL_PROGRAM= \
+	${INSTALL} -c -s -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}
+INSTALL_SCRIPT=	\
+	${INSTALL} -c -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}
+INSTALL_DATA=	\
+	${INSTALL} -c -o ${SHAREOWN} -g ${SHAREGRP} -m ${SHAREMODE}
+INSTALL_MAN=	\
+	${INSTALL} -c -o ${MANOWN} -g ${MANGRP} -m ${MANMODE}
+INSTALL_MACROS=	BSD_INSTALL_PROGRAM="${INSTALL_PROGRAM}" \
+		BSD_INSTALL_SCRIPT="${INSTALL_SCRIPT}" \
+		BSD_INSTALL_DATA="${INSTALL_DATA}" \
+		BSD_INSTALL_MAN="${INSTALL_MAN}"
+MAKE_ENV+=	${INSTALL_MACROS}
+
+PKG_CMD?=	${PKGMK}
+PKG_DELETE?=	${PKGRM}
+
+AWK?=		/usr/bin/awk
+CCSMAKE?=	/usr/ccs/bin/make
+CHOWN?=		/usr/bin/chown
+CP?=		/usr/bin/cp
+ECHO?=		/usr/bin/echo
+ENV?=		/usr/bin/env
+EXPR?=		/usr/bin/expr
+GREP?=		/usr/bin/grep
+GZCAT?=		${LOCALBASE}/bin/gzip -cd
+GZIP?=		${LOCALBASE}/bin/gzip
+INSTALL?=	/usr/ucb/install
+LN?=		/usr/bin/ln
+MV?=		/usr/bin/mv
+MKDIR?=		/usr/bin/mkdir -p
+PKGADD?=	/usr/sbin/pkgadd
+PKGMK?=		/usr/bin/pkgmk -o
+PKGPROTO?=	/usr/bin/pkgproto
+PKGRM?=		/usr/sbin/pkgrm
+PKGTRANS?=	/usr/bin/pkgtrans -o
+PWD?=		/usr/bin/pwd
+RM?=		/usr/bin/rm
+RMDIR?=		/usr/bin/rmdir
+SED?=		/usr/bin/sed
+SH?=		/bin/sh
+UNAME?=		/usr/bin/uname
+WGET?=		${LOCALBASE}/bin/wget
+
+ECHO_MSG?=	${ECHO}
+POSTPROTO=	${PKGBUILDDIR}/tools/postproto.sh
+
+ALL_TARGET?=		all
+INSTALL_TARGET?=	install
+
+BINOWN?=	root
+BINGRP?=	bin
+BINMODE?=	755
+SHAREOWN?=	root
+SHAREGRP?=	bin
+SHAREMODE?=	644
+MANOWN?=	root
+MANGRP?=	bin
+MANMODE?=	644
 
 MASTER_SITES_GNU+=	\
 	ftp://prep.ai.mit.edu/pub/gnu/@SUBDIR@/ \
@@ -120,31 +171,39 @@ MASTER_SITES_GNU+=	\
 MASTER_SITES?=	
 PATCH_SITES?=	
 
-EXTRACT_SUFX?=	.tar.gz
 DISTFILES?=	${DISTNAME}${EXTRACT_SUFX}
 PKGNAME?=	${DISTNAME}
+
+ALLFILES?=	${DISTFILES} ${PATCHFILES}
+
+CKSUMFILES=	${ALLFILES}
+
 EXTRACT_ONLY?=	${DISTFILES}
 
-PATCH_STRIP?=	-p0
-PATCH_DIST_STRIP?=	-p0
-PATCH_ARGS=	-d ${WRKSRC} --backup --forward --quiet -E ${PATCH_STRIP}
-PATCH_DIST_APPLY_DIR?=	${WRKSRC}
-PATCH_DIST_ARGS=	-d ${PATCH_DIST_APPLY_DIR} --backup --forward --quiet -E ${PATCH_DIST_STRIP}
+PKG_MAINTAINER?=	kunishi@c.oka-pu.ac.jp
 
-CONFIGURE_ENV+=	CC=${CC} \
-		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib
+CONFIGURE_SCRIPT?=	configure
+CONFIGURE_TARGET?=	${GNU_HOSTTYPE}
 
-MAKEFILE?=	Makefile
-MAKE_FLAGS?=	-f ${MAKEFILE}
-MAKE_ENV+=	PREFIX=${PREFIX} \
-		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib \
-		CC=${CC}
+.if defined(GNU_CONFIGURE)
+CONFIGURE_ARGS+=	--prefix=${PREFIX} ${CONFIGURE_TARGET}
+HAS_CONFIGURE=		yes
+MAKE_ARGS+=		prefix=${PREFIX}
+MAKE_INSTALL_ARGS+=	prefix=${WRK_BASEDIR}
+.endif
 
-MAKE_INSTALL_ENV+=	PREFIX=${WRK_BASEDIR} \
-		LD_RUN_PATH=${LOCALBASE}/lib:${X11BASE}/lib \
-		CC=${CC}
-MAKE_INSTALL_ARGS+=	INSTALL=${INSTALL}
-MAKE_INSTALL_EXEC_DIR?=	${WRKSRC}
+.if defined(USE_GMAKE)
+MAKE_ENV+=	MAKE=${GMAKE}
+.endif
+
+.if defined(USE_IMAKE)
+MAKE_ARGS+=	DESTDIR=${PREFIX}
+MAKE_INSTALL_ARGS+=	DESTDIR=${WRKDIR} PREFIX=${PREFIX} \
+		LOCALBASE=${LOCALBASE} X11BASE=${X11BASE}
+.if !defined(NO_INSTALL_MAN)
+INSTALL_TARGET+=	install.man
+.endif
+.endif
 
 ### rule definitions
 
@@ -178,6 +237,7 @@ ${EXTRACT_COOKIE}:
 .if target(pre-extract)
 	@${MAKE} pre-extract
 .endif
+	@${MAKE} checksum
 	@${MAKE} do-extract
 .if target(post-extract)
 	@${MAKE} post-extract
@@ -312,7 +372,7 @@ do-extract:
 	@${MKDIR} ${WRKDIR}
 	@for file in ${EXTRACT_ONLY}; do \
 	    ${ECHO_MSG} "===>  Extracting $${file}"; \
-	    cd ${WRKDIR} && ${GTAR} xzf ${DISTDIR}/$${file}; \
+	    cd ${WRKDIR} && ${TAR} xzf ${DISTDIR}/$${file}; \
 	done
 .endif
 
@@ -355,8 +415,12 @@ do-configure:
 	@if [ -f ${SCRIPTDIR}/configure ]; then \
 	  cd ${.CURDIR} && ${SH} ${SCRIPTDIR}/configure; \
 	fi
-.if defined(HAS_CONFIGURE) || defined(USE_IMAKE)
-	@cd ${WRKSRC} && ${ENV} ${CONFIGURE_ENV} ${CONFIGURE} ${CONFIGURE_ARGS}
+.if defined(HAS_CONFIGURE)
+	@cd ${WRKSRC} && \
+	  ${ENV} ${CONFIGURE_ENV} ./${CONFIGURE_SCRIPT} ${CONFIGURE_ARGS}
+.endif
+.if defined(USE_IMAKE)
+	@cd ${WRKSRC} && ${XMKMF})
 .endif
 .endif
 
@@ -412,19 +476,49 @@ do-release:
 .endif
 .endif
 
+.if !target(checksum)
+checksum:
+	@if [ ! -f ${MD5_FILE} ]; then \
+		${ECHO_MSG} ">> No MD5 checksum file."; \
+	else \
+		(cd ${DISTDIR}; OK="true"; \
+		  for file in ${CKSUMFILES}; do \
+			CKSUM=`${MD5} < $$file`; \
+			CKSUM2=`${GREP} "^MD5 ($$file)" ${MD5_FILE} | ${AWK} '{print $$4}'`; \
+			if [ "$$CKSUM2" = "" ]; then \
+				${ECHO_MSG} ">> No checksum recorded for $$file."; \
+				OK="false"; \
+			elif [ "$$CKSUM2" = "IGNORE" ]; then \
+				${ECHO_MSG} ">> Checksum for $$file is set to IGNORE in md5 file even though"; \
+				${ECHO_MSG} "   the file is not in the "'$$'"{IGNOREFILES} list."; \
+				OK="false"; \
+			elif ${EXPR} "$$CKSUM2" : ".*$$CKSUM" > /dev/null; then \
+				${ECHO_MSG} ">> Checksum OK for $$file."; \
+			else \
+				${ECHO_MSG} ">> Checksum mismatch for $$file."; \
+				OK="false"; \
+			fi; \
+		done) \
+	fi
+.endif
+
 ###
 
+.if !target(generate-prototype)
 generate-prototype:
 	@${SED} -e "s?%%PKGDIR%%?${PKGDIR}?g" \
 	    -e 's?%%GNU_HOSTTYPE%%?${GNU_HOSTTYPE}?g' \
 	    -e 's?%%WRK_BASEDIR%%?${WRK_BASEDIR}?g' \
 	    ${PKGDIR}/prototype.in > ${PKGDIR}/prototype
+.endif
 
+.if !target(generate-pkginfo)
 generate-pkginfo:
 	@${SED} -e 's?%%ARCH%%?${ARCH}?' \
 	    -e 's?%%BASEDIR%%?${PREFIX}?' \
 	    -e 's?%%PKG_MAINTAINER%%?${PKG_MAINTAINER}?' \
 	    ${PKGDIR}/pkginfo.in > ${PKGDIR}/pkginfo
+.endif
 
 clean:
 	@${ECHO_MSG} "===> Cleaning for ${PKGNAME}"
@@ -440,6 +534,17 @@ distclean:	pkgclean
 	 ${RM} -rf ${DISTDIR}/$${file}; done
 
 ### only for maintainance
+.if !target(makesum)
+makesum:	fetch
+	@${MKDIR} ${FILESDIR}
+	@if [ -f ${MD5_FILE} ]; then ${RM} -f ${MD5_FILE}; fi
+	@cd ${DISTDIR}; \
+	  for file in ${CKSUMFILES}; do \
+		${MD5} $$file >> ${MD5_FILE}; \
+	  done
+.endif
+
+.if !target(gen-prototype-in)
 gen-prototype-in:	${INSTALL_COOKIE}
 	@${MAKE} install
 	@${ECHO_MSG} "===> Building prototype.in"
@@ -452,7 +557,9 @@ gen-prototype-in:	${INSTALL_COOKIE}
 	 | ${POSTPROTO} > ${PKGDIR}/prototype.in
 	@${ECHO_MSG} "===> prototype.in template was successfully made."
 	@${ECHO_MSG} "===> You must edit the file by hand."
+.endif
 
+.if !target(gen-instinfo)
 gen-instinfo:
 .if defined(INST_INFO_FILES)
 	@${MKDIR} ${PKGDIR}
@@ -462,4 +569,5 @@ gen-instinfo:
 	${TOOLSDIR}/gen-info-preremove.sh "${INST_INFO_FILES}" >> ${PKGDIR}/preremove
 .else
 	@${ECHO_MSG} ">> Define INST_INFO_FILES definitely."
+.endif
 .endif
