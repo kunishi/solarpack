f58d4a9ff340c1f3d2073276fbfb8b2a41733e37
kunishi <devnull@localhost>
2000-06-05 09:01:50 +0000

    new file for SUBDIR.

---
+++ ports/Mk/port.subdir.mk
@@ -0,0 +1,58 @@
+#
+# $Id: port.subdir.mk,v 1.1 2000/06/05 08:58:38 kunishi Exp $
+#
+
+.MAIN:	all
+
+.if !defined(ARCH)
+ARCH!=	/usr/bin/mach
+.endif
+.if !defined(OSREL)
+OSREL!=	/usr/bin/uname -r
+.endif
+.if !defined(OSREL_SOLARIS)
+OSREL_SOLARIS!=	/usr/bin/uname -r | /usr/bin/sed 's/^5/2/'
+.endif
+.if !defined(SUNW_ISA)
+SUNW_ISA!=	/usr/bin/uname -p
+.endif
+
+ECHO_MSG?=	/usr/bin/echo
+
+TARGETS+=	all
+TARGETS+=	build
+TARGETS+=	checksum
+TARGETS+=	clean
+TARGETS+=	configure
+TARGETS+=	distclean
+TARGETS+=	extract
+TARGETS+=	fetch
+TARGETS+=	install
+TARGETS+=	instpkg
+TARGETS+=	package
+TARGETS+=	pkgclean
+
+.for __target in ${TARGETS}
+.if !target(${__target})
+${__target}:	_SUBDIRUSE
+.endif
+.endfor
+
+_SUBDIRUSE:	.USE
+.if defined(SUBDIR)
+	@for entry in ${SUBDIR}; do \
+	  (set -e; if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
+	    _newdir_="$${entry}.${MACHINE}"; \
+	  else \
+	    _newdir_="$${entry}"; \
+	  fi; \
+	  if test X"${_THISDIR_}" = X""; then \
+	    _nextdir_="$${_newdir_}"; \
+	  else \
+	    _nextdir_="$${_THISDIR_}/$${_newdir_}"; \
+	  fi; \
+	  ${ECHO_MSG} "===> $${_nextdir_}"; \
+	  cd ${.CURDIR}/$${_newdir_}; \
+	  ${MAKE} _THISDIR_="$${_nextdir_}" ${.TARGET}); \
+	done
+.endif
