0e52258c989a427913759517466928cc3e481b6f
kunishi <devnull@localhost>
2000-06-06 04:51:29 +0000

    tiny bug fixed.

--- src/version.mk
+++ src/version.mk
@@ -1,5 +1,5 @@
 #
-# $Id: version.mk,v 1.5 2000/05/17 05:41:47 kunishi Exp $
+# $Id: version.mk,v 1.6 2000/06/06 04:51:29 kunishi Exp $
 #
-SOAP_VERSION=	0.3.1
-SOAP_REV=	2
+SOAP_VERSION=	0.3.2
+SOAP_REV=	3
