6d5b55b57843f17b8ebe9b1c14dc296be1728e90
kunishi <devnull@localhost>
2001-02-22 13:04:24 +0000

    more brush up.

--- src/bootstrap/config.mk
+++ src/bootstrap/config.mk
@@ -1,34 +1,43 @@
 #
 # Makefile configuration file for standard Solaris make
-# $Id: config.mk,v 1.5 2001/02/22 08:38:01 kunishi Exp $
+# $Id: config.mk,v 1.6 2001/02/22 13:04:21 kunishi Exp $
 #
 
 WORKDIR=	${TOPDIR}/work
 DISTDIR=	${TOPDIR}/distfiles
-LOCALBASE=	/usr/pkg
+LOCALBASE?=	/usr/pkg
 PREFIX=		${LOCALBASE}
 TMPPREFIX=	${WORKDIR}${LOCALBASE}
 BOOTSTRAPDIR=	${TOPDIR}/bootstrap
 
+CC?=		gcc
+CXX?=		c++
 ECHO_MSG=	echo
 ENV=		env
 EXTRACT_CMD=	tar xf
+GNUPATCH=	gpatch
+GUNZIP=		gzip -c -d
 MAKE?=		make
 MKDIR=		mkdir -p
 PATCH=		patch
 TOUCH=		touch
+
+PKGMK=		pkgmk -o
+PKGPROTO=	pkgproto
+PKGTRANS=	pkgtrans -o
+
 DO_NADA=	true
+GNU_CONFIGURE=	./configure
 INSTALL_PROGRAM=	/usr/ucb/install -c -m 755
-
-GUNZIP=		gzip -c -d
-GNUPATCH=	gpatch
-
-CC?=		gcc
 
 PATCH_COOKIE=		.patch_done
 CONFIGURE_COOKIE=	.configure_done
 BUILD_COOKIE=		.build_done
 INSTALL_COOKIE=		.install_done
+
+BUILD_TARGET=		# none
+INSTALL_TARGET=		install
+CLEAN_TARGET=		clean
 
 CONFIGURE_ENV=	LANG=C
 MAKE_ENV=	TOPDIR=${TOPDIR} \
@@ -36,6 +45,8 @@ MAKE_ENV=	TOPDIR=${TOPDIR} \
 		LOCALBASE=${LOCALBASE} \
 		PATH=${PATH}
 INSTALL_ENV=	${MAKE_ENV}
+
+CONFIGURE_ARGS=	--prefix=${PREFIX}
 MAKE_INSTALL_ARGS=	PREFIX=${TMPPREFIX} prefix=${TMPPREFIX}
 
-MAINTAINER=	kunishi@c.oka-pu.ac.jp
+MAINTAINER=	kunishi@acm.org
--- src/bootstrap/rule.mk
+++ src/bootstrap/rule.mk
@@ -1,6 +1,6 @@
 #
 # common rule for soap, standard Solaris make version
-# $Id: rule.mk,v 1.3 2001/02/22 08:38:02 kunishi Exp $
+# $Id: rule.mk,v 1.4 2001/02/22 13:04:21 kunishi Exp $
 #
 
 .PHONY:	patch configure build install
@@ -8,20 +8,20 @@
 install:=	PREFIX=${TMPPREFIX}
 
 ${PATCH_COOKIE}:
-	${MAKE_ENV} ${MAKE} ${MAKEFLAGS} patch
-	${TOUCH} ${PATCH_COOKIE}
+	@${MAKE_ENV} ${MAKE} ${MAKEFLAGS} patch
+	@${TOUCH} ${PATCH_COOKIE}
 
 ${CONFIGURE_COOKIE}:	${PATCH_COOKIE}
-	${MAKE_ENV} ${MAKE} ${MAKEFLAGS} configure
-	${TOUCH} ${CONFIGURE_COOKIE}
+	@${MAKE_ENV} ${MAKE} ${MAKEFLAGS} configure
+	@${TOUCH} ${CONFIGURE_COOKIE}
 
 ${BUILD_COOKIE}:	${CONFIGURE_COOKIE}
-	${MAKE_ENV} ${MAKE} ${MAKEFLAGS} build
-	${TOUCH} ${BUILD_COOKIE}
+	@${MAKE_ENV} ${MAKE} ${MAKEFLAGS} build
+	@${TOUCH} ${BUILD_COOKIE}
 
 ${INSTALL_COOKIE}:	${BUILD_COOKIE}
-	${MAKE_ENV} ${MAKE} ${MAKEFLAGS} install
-	${TOUCH} ${INSTALL_COOKIE}
+	@${MAKE_ENV} ${MAKE} ${MAKEFLAGS} install
+	@${TOUCH} ${INSTALL_COOKIE}
 
 clean::
-	${RM} ${PATCH_COOKIE} ${CONFIGURE_COOKIE} ${BUILD_COOKIE} ${INSTALL_COOKIE}
+	@${RM} ${PATCH_COOKIE} ${CONFIGURE_COOKIE} ${BUILD_COOKIE} ${INSTALL_COOKIE}
