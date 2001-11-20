a7bc3d78c8d152fa9218b0edeeb9b57675c78317
kunishi <devnull@localhost>
2001-11-20 14:49:26 +0000

    sync up with 10.0g.

--- ports/elisp/auctex/files/auctex-setup.el
+++ ports/elisp/auctex/files/auctex-setup.el
@@ -1,14 +1,12 @@
+;;; 
+;;; auctex-setup.el
+;;; 
+
 (require 'tex-site)
+
 (japanese-latex-mode)
-(setq TeX-command-list
-      (append
-       (list (list "LaTeX2e" "platex '\\nonstopmode\\input{%t}'"
-		   'TeX-run-LaTeX nil t)
-	     (list "LaTeX" "platex209 '\\nonstopmode\\input{%t}'"
-		   'TeX-run-LaTeX nil t))
-       TeX-command-list))
-(setq auto-mode-alist
-      (cons (cons "\\.tex" 'japanese-latex-mode) auto-mode-alist))
-(setq TeX-printer-list '(("copy") ("ps")))
+(add-to-list 'auto-mode-alist
+             (cons "\\.tex" 'japanese-latex-mode))
+(setq TeX-printer-list '(("xerox")))
 
 (provide 'auctex-setup)
